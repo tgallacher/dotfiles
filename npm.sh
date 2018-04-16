6e8fe95fb70b1a232ba45a8aa5a095721ccc85e2
Tom Gallacher <thomas.gallacher@gmail.com>
2018-04-16 23:18:38 +0100

    Add vscodeExtension, todo list scraper + Yeoman tools to NPM install

--- npm.sh
+++ npm.sh
@@ -4,13 +4,16 @@
 which npm > /dev/null
 if [ ! ($? -eq 0) ]; then
     echo '>> NOTE: "npm" not found on $PATH';
-    return;
+    exit 1;
 fi;
 
 packages=(
     nodemon
     flow-bin
     flow-coverage-report
+    leasot # todo list scraper
+    vsce # VS Code extension cli
+    yo # Yeoman
 )
 
 npm install -g "${packages[@]}"
