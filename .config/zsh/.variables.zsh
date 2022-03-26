af1a7f6a645863725d9b30b8608d9a2d7b0f29b1
Tom Gallacher <tgallacher@users.noreply.github.com>
2022-03-26 14:44:45 +0000

    fix(zsh): remove duplicate / redundant env vars

--- .config/zsh/.variables.zsh
+++ .config/zsh/.variables.zsh
@@ -1,11 +1,2 @@
-#
-export TERM=xterm-256color
-
-# You may need to manually set your language environment
-export LANG=en_GB.UTF-8
-
-# Preferred editor for local and remote sessions
-export EDITOR='vim'
-
 # Tell homebrew to not autoupdate every single time I run it (just once a week).
 export HOMEBREW_AUTO_UPDATE_SECS=604800
