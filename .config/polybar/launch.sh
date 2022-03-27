b0d8ef298745af52ba610c4b50859f841e461edc
Tom Gallacher <tgallacher@users.noreply.github.com>
2022-03-27 18:58:21 +0100

    feat(polybar): add log file to polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env sh
 
-## Add this to your wm startup file.
-
 # Terminate already running bar instances
 killall -q polybar
 
@@ -9,4 +7,4 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar
-polybar -c ~/.config/polybar/config.ini main &
+polybar -c ~/.config/polybar/config.ini main 2>&1 | tee -a /tmp/polybar-main.log & disown
