9915376881003bb0105041dbcb7c45a291c9be95
Tom Gallacher <tgallacher@users.noreply.github.com>
2022-03-28 23:09:09 +0100

    feat(polybar): add app switcher to launcher

--- .config/polybar/scripts/launcher.sh
+++ .config/polybar/scripts/launcher.sh
@@ -2,4 +2,4 @@
 
 FILE="$HOME/.config/polybar/scripts/rofi/colors.rasi"
 
-rofi -no-config -no-lazy-grab -show drun -modi drun -theme ~/.config/polybar/scripts/rofi/launcher.rasi
+rofi -no-config -no-lazy-grab -show drun -modi drun,window -theme ~/.config/polybar/scripts/rofi/launcher.rasi
