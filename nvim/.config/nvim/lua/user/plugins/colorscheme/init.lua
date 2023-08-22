36f49c793899c0caef93bd811901178c27dae89e
Tom Gallacher <tgallacher@users.noreply.github.com>
2023-08-22 13:25:39 +0100

    feat(nvim): switch theme to nightfly

--- nvim/.config/nvim/lua/user/plugins/colorscheme/init.lua
+++ nvim/.config/nvim/lua/user/plugins/colorscheme/init.lua
@@ -1,7 +1,8 @@
 -- 
 -- Plugin: "cocopon/iceberg.vim"
 --
-local colorscheme = "iceberg"
+-- local colorscheme = "iceberg"
+local colorscheme = "nightfly"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
