7c58b8355f80722476d77ac9cd22f5b7d86d7443
Tom Gallacher <tgallacher@users.noreply.github.com>
2023-08-23 22:43:19 +0100

    chore(nvim): commenting

--- nvim/.config/nvim/lua/user/plugins/git-blame.lua
+++ nvim/.config/nvim/lua/user/plugins/git-blame.lua
@@ -1,3 +1,9 @@
-require("gitblame").setup({
-  enabled = false,
+local status_ok, gitblame = pcall(require, "gitblame")
+if not status_ok then
+  vim.notify "Failed to load gitblame plugin.."
+  return
+end
+
+gitblame.setup({
+  enabled = false, -- use keymap to view blame
 })
