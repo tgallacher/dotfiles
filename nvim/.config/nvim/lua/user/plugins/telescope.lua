df3da586e9d890546622f4108cb7c2941aaddafe
Tom Gallacher <tgallacher@users.noreply.github.com>
2023-08-31 23:47:45 +0100

    chore(nvim): new bindings telescope

--- nvim/.config/nvim/lua/user/plugins/telescope.lua
+++ nvim/.config/nvim/lua/user/plugins/telescope.lua
@@ -19,6 +19,7 @@ telescope.setup {
         ["<C-k>"] = actions.move_selection_previous, -- move to prev result
         ["<C-j>"] = actions.move_selection_next, -- move to next result
         ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
+        ["C-h"] = "which_key",
       },
     },
   },
@@ -44,7 +45,7 @@ telescope.setup {
 }
 
 -- Enable telescope fzf native, if installed
-pcall(require('telescope').load_extension, 'fzf')
+pcall(require('telescope').load_extension, "fzf-native")
 
 ----------------------------------------------------------
 -- KEYMAPS --
@@ -54,13 +55,17 @@ local opts = { noremap = true, silent = true }
 vim.keymap.set("n", "<leader>f", "<Nop>", opts)
 
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
-vim.keymap.set("n", "<leader>fa", function() builtin.find_files({ no_ignore=true, follow=true }) end, { desc = "Find all files"} )
+vim.keymap.set("n", "<leader>fa", function() builtin.find_files({ no_ignore=true, follow=true, hidden=false }) end, { desc = "Find all files"} )
 vim.keymap.set("n", "<leader>fw", builtin.live_grep, { desc = "Find text in cwd" })
+vim.keymap.set("n", "<leader>fs", builtin.grep_string, { desc = "Find text under cursor in cwd" })
 vim.keymap.set("n", "<leader>fg", builtin.git_files, { desc = "Find file known to git" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffer" })
 vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "Find previously opened file"})
 vim.keymap.set("n", "<leader>fq", builtin.quickfix, { desc = "Find in quickfix list"})
-vim.keymap.set("n", "<leader>fz", builtin.current_buffer_fuzzy_find, { desc = "Find text inside current buffer"})
+vim.keymap.set("n", "<leader>fib", builtin.current_buffer_fuzzy_find, { desc = "Find text inside current buffer"})
+vim.keymap.set("n", "<leader>gb", builtin.git_bcommits, { desc = "List git commits for current buffer"})
+vim.keymap.set("n", "<leader>gss", builtin.git_stash, { desc = "List git stashes"})
+vim.keymap.set("n", "<leader>th", builtin.colorscheme, { desc = "List/preview colorschemes"})
 
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help"})
 
