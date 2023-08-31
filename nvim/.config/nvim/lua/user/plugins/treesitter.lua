7ba68100e505ccc7fc8ffe307612003627c0a02a
Tom Gallacher <tgallacher@users.noreply.github.com>
2023-08-31 23:48:58 +0100

    feat(nvim): add zmk syntax + formatting

--- nvim/.config/nvim/lua/user/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/user/plugins/treesitter.lua
@@ -1,49 +1,51 @@
--- 
+--
 -- Plugin: "nvim-treesitter/nvim-treesitter"
 --
 local status, treesitter = pcall(require, "nvim-treesitter.configs")
 if not status then
-  return
+	vim.notify("Failed to load treesitter")
+	return
 end
 
-treesitter.setup {
-  ensure_installed = {          -- put the language you want in this array
-    "lua", 
-    "markdown", 
-    "markdown_inline", 
-    "bash", 
-    "javascript",
-    "typescript",
-    "jsdoc",
-    "graphql",
-    "vimdoc",
-    "terraform",
-    "dockerfile",
-    "nix",
-    "yaml",
-    "prisma",
-    "json",
-    "tsx",
-    "vim",
-    "gitignore",
-  }, 
-  highlight = {
-    enable = true,       -- false will disable the whole extension
-    disable = { "css" }, -- list of language that will be disabled
-  },
-  autopairs = { enable = true },
-  autotag = { enable = true },
-  indent = { 
-    enable = true, 
-    disable = { "python", "css" } 
-  },
-  context_commentstring = {
-    enable = true,
-    enable_autocmd = false,
-  },
-  -- ensure_installed = "all", -- one of "all" or a list of languages
-  -- ignore_install = { "" },                                                       -- List of parsers to ignore installing
-  sync_install = false,                                                          -- install languages synchronously (only applied to `ensure_installed`)
-  auto_install = true,
-}
-
+treesitter.setup({
+	ensure_installed = { -- put the language you want in this array
+		"lua",
+		"markdown",
+		"markdown_inline",
+		"bash",
+		"javascript",
+		"typescript",
+		"jsdoc",
+		"graphql",
+		"vimdoc",
+		"terraform",
+		"dockerfile",
+		"nix",
+		"yaml",
+		"prisma",
+		"json",
+		"tsx",
+		"vim",
+		"gitignore",
+		"kconfig",
+		"devicetree",
+	},
+	highlight = {
+		enable = true, -- false will disable the whole extension
+		disable = { "css" }, -- list of language that will be disabled
+	},
+	autopairs = { enable = true },
+	autotag = { enable = true },
+	indent = {
+		enable = true,
+		disable = { "python", "css" },
+	},
+	context_commentstring = {
+		enable = true,
+		enable_autocmd = false,
+	},
+	-- ensure_installed = "all", -- one of "all" or a list of languages
+	-- ignore_install = { "" },  -- List of parsers to ignore installing
+	sync_install = true, -- install languages synchronously (only applied to `ensure_installed`)
+	auto_install = true,
+})
