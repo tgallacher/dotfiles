7a7f5ba41ed5059dad77bd7a5b5d359d8d95b371
Tom Gallacher <tgallacher@users.noreply.github.com>
2023-09-04 14:22:28 +0100

    chore(nvim): add missing telescope keybinds

--- nvim/.config/nvim/lua/user/plugins/editor.lua
+++ nvim/.config/nvim/lua/user/plugins/editor.lua
@@ -236,41 +236,46 @@ return {
     },
     -- stylua: ignore
     keys = {
-      { "<leader>,",  "<cmd>Telescope buffers show_all_buffers=true<cr>", desc = "Switch Buffer" },
+      { "<leader>,",  "<cmd>Telescope buffers show_all_buffers=true<cr>",     desc = "Switch Buffer" },
       -- { "<leader>/", Util.telescope("live_grep"), desc = "Grep (root dir)", },
-      { "<leader>:",  "<cmd>Telescope command_history<cr>",               desc = "Command History", },
+      { "<leader>:",  "<cmd>Telescope command_history<cr>",                   desc = "Command History", },
       -- { "<leader><space>", Util.telescope("files"), desc = "Find Files (root dir)", },
       -- find
-      { "<leader>fb", "<cmd>Telescope buffers<cr>",                       desc = "Buffers" },
-      -- { "<leader>ff", Util.telescope("files"), desc = "Find Files (root dir)", },
-      -- { "<leader>fF", Util.telescope("files", { cwd = false }), desc = "Find Files (cwd)", },
-      { "<leader>fr", "<cmd>Telescope oldfiles<cr>",                      desc = "Recent" },
-      -- { "<leader>fR", Util.telescope("oldfiles", { cwd = vim.loop.cwd() }), desc = "Recent (cwd)", },
+      { "<leader>fb", "<cmd>Telescope buffers<cr>",                           desc = "Buffers" },
+      { "<leader>ff", "<cmd>Telescope find_files<cr>",                        desc = "Find Files (root dir)" },
+      { "<leader>fF", "<cmd>Telescope find_files cwd=true",                   desc = "Find Files (cwd)", },
+      { "<leader>fr", "<cmd>Telescope oldfiles<cr>",                          desc = "Recent" },
+      { "<leader>fR", "<cmd>Telescope oldfiles cwd=vim.loop.cwd()",           desc = "Recent (cwd)", },
       -- git
-      { "<leader>gc", "<cmd>Telescope git_commits<CR>",                   desc = "commits" },
-      { "<leader>gs", "<cmd>Telescope git_status<CR>",                    desc = "status" },
+      { "<leader>gc", "<cmd>Telescope git_bcommits<CR>",                      desc = "commits" },
+      { "<leader>gs", "<cmd>Telescope git_status<CR>",                        desc = "status" },
       -- search
-      { '<leader>s"', "<cmd>Telescope registers<cr>",                     desc = "Registers", },
-      { "<leader>sa", "<cmd>Telescope autocommands<cr>",                  desc = "Auto Commands", },
-      { "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>",     desc = "Buffer" },
-      { "<leader>sc", "<cmd>Telescope command_history<cr>",               desc = "Command History", },
-      { "<leader>sC", "<cmd>Telescope commands<cr>",                      desc = "Commands" },
-      { "<leader>sd", "<cmd>Telescope diagnostics bufnr=0<cr>",           desc = "Document diagnostics", },
-      { "<leader>sD", "<cmd>Telescope diagnostics<cr>",                   desc = "Workspace diagnostics", },
-      -- { "<leader>sg", Util.telescope("live_grep"), desc = "Grep (root dir)", },
-      -- { "<leader>sG", Util.telescope("live_grep", { cwd = false }), desc = "Grep (cwd)", },
-      { "<leader>sh", "<cmd>Telescope help_tags<cr>",                     desc = "Help Pages", },
-      { "<leader>sH", "<cmd>Telescope highlights<cr>",                    desc = "Search Highlight Groups", },
-      { "<leader>sk", "<cmd>Telescope keymaps<cr>",                       desc = "Key Maps" },
-      { "<leader>sM", "<cmd>Telescope man_pages<cr>",                     desc = "Man Pages", },
-      { "<leader>sm", "<cmd>Telescope marks<cr>",                         desc = "Jump to Mark", },
-      { "<leader>so", "<cmd>Telescope vim_options<cr>",                   desc = "Options" },
-      { "<leader>sR", "<cmd>Telescope resume<cr>",                        desc = "Resume" },
-      -- { "<leader>sw", Util.telescope("grep_string", { word_match = "-w" }), desc = "Word (root dir)", },
-      -- { "<leader>sW", Util.telescope("grep_string", { cwd = false, word_match = "-w" }), desc = "Word (cwd)", },
-      -- { "<leader>sw", Util.telescope("grep_string"), mode = "v", desc = "Selection (root dir)", },
-      -- { "<leader>sW", Util.telescope("grep_string", { cwd = false }), mode = "v", desc = "Selection (cwd)", },
-      -- { "<leader>uC", Util.telescope("colorscheme", { enable_preview = true }), desc = "Colorscheme with preview", },
+      { '<leader>s"', "<cmd>Telescope registers<cr>",                         desc = "Registers", },
+      { "<leader>sa", "<cmd>Telescope autocommands<cr>",                      desc = "Auto Commands", },
+      { "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>",         desc = "Buffer" },
+      { "<leader>sc", "<cmd>Telescope command_history<cr>",                   desc = "Command History", },
+      { "<leader>sC", "<cmd>Telescope commands<cr>",                          desc = "Commands" },
+      { "<leader>sd", "<cmd>Telescope diagnostics bufnr=0<cr>",               desc = "Document diagnostics", },
+      { "<leader>sD", "<cmd>Telescope diagnostics<cr>",                       desc = "Workspace diagnostics", },
+      { "<leader>sg", "<cmd>Telescope live_grep",                             desc = "Grep (root dir)", },
+      { "<leader>sG", "<cmd>Telescope live_grep cwd=false",                   desc = "Grep (cwd)", },
+      { "<leader>sh", "<cmd>Telescope help_tags<cr>",                         desc = "Help Pages", },
+      { "<leader>sH", "<cmd>Telescope highlights<cr>",                        desc = "Search Highlight Groups", },
+      { "<leader>sk", "<cmd>Telescope keymaps<cr>",                           desc = "Key Maps" },
+      { "<leader>sM", "<cmd>Telescope man_pages<cr>",                         desc = "Man Pages", },
+      { "<leader>sm", "<cmd>Telescope marks<cr>",                             desc = "Jump to Mark", },
+      { "<leader>so", "<cmd>Telescope vim_options<cr>",                       desc = "Options" },
+      { "<leader>sR", "<cmd>Telescope resume<cr>",                            desc = "Resume" },
+      { "<leader>sw", "<cmd>Telescope grep_string word_match='-w'",           desc = "Word (root dir)", },
+      { "<leader>sW", "<cmd>Telescope grep_string cwd=false,word_match='-w'", desc = "Word (cwd)", },
+      { "<leader>sw", "<cmd>Telescope grep_string mode='v'",                  desc = "Selection (root dir)", },
+      {
+        "<leader>sW",
+        "<cmd>Telescope grep_string cwd=false",
+        mode = "v",
+        desc = "Selection (cwd)",
+      },
+      { "<leader>uC", "<cmd>Telescope colorscheme enable_preview=true", desc = "Colorscheme with preview", },
       -- {
       -- 	"<leader>ss",
       -- 	Util.telescope("lsp_document_symbols", {
@@ -330,14 +335,14 @@ return {
             --   local line = action_state.get_current_line()
             --   Util.telescope("find_files", { hidden = true, default_text = line })()
             -- end,
-            -- ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-            -- ["<C-j>"] = actions.move_selection_next, -- move to next result
             -- ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
             ["<C-j>"] = function(...)
-              return require("telescope.actions").cycle_history_next(...)
+              -- return require("telescope.actions").cycle_history_next(...)
+              return require("telescope.actions").move_selection_previous
             end,
             ["<C-k>"] = function(...)
-              return require("telescope.actions").cycle_history_prev(...)
+              -- return require("telescope.actions").cycle_history_prev(...)
+              return require("telescope.actions").move_selection_next
             end,
             ["<C-f>"] = function(...)
               return require("telescope.actions").preview_scrolling_down(...)
@@ -358,8 +363,10 @@ return {
         },
       },
     },
-    config = function()
+    config = function(_, opts)
       local telescope = require("telescope")
+
+      telescope.setup(opts)
       telescope.load_extension("fzf")
     end,
   },
