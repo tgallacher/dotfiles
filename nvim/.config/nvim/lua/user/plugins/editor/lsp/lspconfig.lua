7e3ea4d93f9af36801f2c40d6d248a282c862e2a
Tom Gallacher <tgallacher@users.noreply.github.com>
2023-09-12 09:59:45 +0100

    chore(nvim): update lsp keymaps

--- nvim/.config/nvim/lua/user/plugins/editor/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/user/plugins/editor/lsp/lspconfig.lua
@@ -18,12 +18,11 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     dependencies = {
       { "folke/neoconf.nvim",      cmd = "Neoconf", config = true },
-      { "j-hui/fidget.nvim",       config = true,   tag = "legacy" },
+      -- { "j-hui/fidget.nvim",       config = true,   tag = "legacy" },
       { "smjonas/inc-rename.nvim", config = true },
       "williamboman/mason.nvim",
       "williamboman/mason-lspconfig.nvim",
       "jay-babu/mason-null-ls.nvim",
-
       "hrsh7th/cmp-nvim-lsp",
     },
     config = function()
@@ -38,32 +37,29 @@ return {
         local opts = { noremap = true, silent = true, buffer = bufnr }
         local keymap = vim.keymap.set
 
-        keymap("n", "gf", "<cmd>Lspsaga lsp_finder<CR>", opts)                                                     -- show definition, references
-        -- stylua: ignore
-        keymap("n", "gd", function() require("telescope.builtin").lsp_definitions({ reuse_win = true }) end, opts) -- got to declaration
-        keymap("n", "gD", "<cmd>Lspsaga peek_definition<CR>", opts)                                                -- see definition and make edits in window
-        -- stylua: ignore
-        keymap("n", "gi", function() require("telescope.builtin").lsp_implementations({ reuse_win = true }) end, opts) -- go to implementation
-        -- stylua: ignore
-        keymap("gt", function() require("telescope.builtin").lsp_type_definitions({ reuse_win = true }) end, opts) -- goto type definition
-        keymap("n", "<localleader>rn", "<cmd>Lspsaga rename<CR>", opts)                                                 -- smart rename
-        keymap("n", "<localleader>D", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)                                   -- show  diagnostics for line
-        keymap("n", "<localleader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts)                                 -- show diagnostics for cursor
-        -- keymap("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts) -- jump to previous diagnostic in buffer
-        -- keymap("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts) -- jump to next diagnostic in buffer
+        keymap("n", "gf", "<cmd>Lspsaga lsp_finder<CR>", opts)                            -- show definition, references
+        keymap("n", "gd", "<cmd>Telescope lsp_definitions reuse_win=true<cr>", opts)      -- got to declaration
+        keymap("n", "gD", "<cmd>Lspsaga peek_definition<CR>", opts)                       -- see definition and make edits in window
+        keymap("n", "gi", "<cmd>Telescope lsp_implementations reuse_win=true<cr>", opts)  -- go to implementation
+        keymap("n", "gR", "<cmd>Telescope lsp_references reuse_win=true<cr>", opts)  -- go to implementation
+        keymap("n", "gt", "<cmd>Telescope lsp_type_definitions reuse_win=true<cr>", opts) -- goto type definition
+        keymap("n", "<localleader>rn", "<cmd>Lspsaga rename<CR>", opts)                   -- smart rename
+        keymap("n", "<localleader>D", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)     -- show  diagnostics for line
+        keymap("n", "<localleader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts)   -- show diagnostics for cursor
+
+        -- stylua: ignore start
         keymap("]d", function() lsputils.diagnostic_goto(true) end, { desc = "Next Diagnostic" })
         keymap("[d", function() lsputils.diagnostic_goto(false) end, { desc = "Prev Diagnostic" })
         keymap("]e", function() lsputils.diagnostic_goto(true, "ERROR") end, { desc = "Next Error" })
         keymap("[e", function() lsputils.diagnostic_goto(false, "ERROR") end, { desc = "Prev Error" })
         keymap("]w", function() lsputils.diagnostic_goto(true, "WARNING") end, { desc = "Next Warning" })
         keymap("[w", function() lsputils.diagnostic_goto(false, "WARNING") end, { desc = "Prev Warning" })
+        -- stylua: ignore end
+
         -- keymap("n", "<localleader>o", "<cmd>LSoutlineToggle<CR>", opts) -- see outline on right hand side
-        -- stylua: ignore
-        -- keymap({ "n", "v" }, "<leader>fm", function() vim.lsp.buf.format { async = true } end, opts)
         keymap({ "n", "v" }, "<localleader>ca", "<cmd>Lspsaga code_action<cr>", opts)
-        keymap("n", "<localleader>ls", function() require("telescope.builtin").lsp_document_symbols() end, opts)                                                                                                  -- document symbols
-        -- stylua: ignore
-        keymap("n", "<localleader>ls", function() require("telescope.builtin").lsp_dynamic_workspace_symbols() end, opts) -- workspace symbols
+        keymap("n", "<localleader>gs", "<cmd>Telescope lsp_document_symbols<cr>", opts)          -- document symbols
+        keymap("n", "<localleader>gS", "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", opts) -- workspace symbols
 
         -- typescript specific keymaps (e.g. rename file and update imports)
         if client.name == "tsserver" then
@@ -75,10 +71,7 @@ return {
         end
 
         local status_ok, illuminate = pcall(require, "illuminate")
-        if        status_ok then
-          
-          illuminate.on_attach(client)
-        end
+        if status_ok then illuminate.on_attach(client) end
       end
 
       -- Configure LSP servers
@@ -87,18 +80,14 @@ return {
         server = vim.split(server, "@")[1]
 
         -- merge overrides if defined
-        if lsp.serverConfigs[server] then
-          opts = vim.tbl_deep_extend("force", lsp.serverConfigs[server], opts)
-        end
+        if lsp.serverConfigs[server] then opts = vim.tbl_deep_extend("force", lsp.serverConfigs[server], opts) end
 
         -- TS server doesn't follow the std config syntax :|
-        if server == "tsserver" then
-          opts = { server = opts }
-        end
+        if server == "tsserver" then opts = { server = opts } end
         lspconfig[server].setup(opts)
       end
 
-      -- configure Diagnostics 
+      -- configure Diagnostics
       local icons = require "user.config.icons"
       local signs = {
         { name = "DiagnosticSignError", text = icons.diagnostics.Error },
