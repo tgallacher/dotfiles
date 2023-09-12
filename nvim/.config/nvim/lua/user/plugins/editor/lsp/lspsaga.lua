26c4e2069bc9f3f9c1d91bb76b864032418eb0f6
Tom Gallacher <tgallacher@users.noreply.github.com>
2023-09-12 08:20:23 +0100

    style(nvim): formatting

--- nvim/.config/nvim/lua/user/plugins/editor/lsp/lspsaga.lua
+++ nvim/.config/nvim/lua/user/plugins/editor/lsp/lspsaga.lua
@@ -1,40 +1,43 @@
 local icons = require "user.config.icons"
 
 return {
-	-- LSP UI
-	{
-		"glepnir/lspsaga.nvim",
+  -- LSP UI
+  {
+    "glepnir/lspsaga.nvim",
     event = "LspAttach",
-		branch = "main",
-		-- main = "lspsaga",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-			"nvim-tree/nvim-web-devicons",
-		},
-		opts = {
-			-- keybinds for navigation in lspsaga window
-			scroll_preview = { scroll_down = "<C-f>", scroll_up = "<C-b>" },
-			-- use enter to open file with definition preview
-			definition = {
-				edit = "<CR>",
-			},
-      lightbulb = {
-        virtual_text = false,
+    branch = "main",
+    -- main = "lspsaga",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      symbol_in_winbar = {
+        enable = false,
       },
-			ui = {
-        code_action = icons.ui.Lightbulb, 
+      -- keybinds for navigation in lspsaga window
+      scroll_preview = { scroll_down = "<C-f>", scroll_up = "<C-b>" },
+      -- use enter to open file with definition preview
+      definition = {
+        edit = "<CR>",
+      },
+      lightbulb = { virtual_text = false },
+      ui = {
+        code_action = icons.ui.Lightbulb,
+        expand = icons.ui.ArrowRight,
+        collapse = icons.ui.ArrowDown,
         colors = {
-					normal_bg = "#022746",
-				},
-			},
-			floaterm = {
-				width = 0.9,
-				height = 0.9,
-			},
-		},
+          normal_bg = "#022746",
+        },
+      },
+      floaterm = {
+        width = 0.9,
+        height = 0.9,
+      },
+    },
     config = true,
-		-- keys = {
-		-- 	{ "<A-d>", "<cmd>Lspsaga term_toggle<CR>", mode = { "n", "t" } },
-		-- },
-	},
+    -- keys = {
+    -- 	{ "<A-d>", "<cmd>Lspsaga term_toggle<CR>", mode = { "n", "t" } },
+    -- },
+  },
 }
