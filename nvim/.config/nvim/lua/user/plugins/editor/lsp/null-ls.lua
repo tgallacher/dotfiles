92e02c74aacf5c06d37d8cf5c4d26e840e137a74
Tom Gallacher <tgallacher@users.noreply.github.com>
2023-09-12 07:40:23 +0100

    fix(nvim): null ls set up

--- nvim/.config/nvim/lua/user/plugins/editor/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/user/plugins/editor/lsp/null-ls.lua
@@ -3,25 +3,34 @@ return {
 	{
 		"jose-elias-alvarez/null-ls.nvim",
 		-- main = "null-ls",
-		opts = {
-			debug = false,
-			-- sources = {
-			-- 		require("null-ls").builtins.formatting.prettier,
-					-- require("null-ls").builtins.formatting.stylua,
-   --        require("null-ls").builtins.diagnostics.eslint_d.with({ -- js/ts linter
-			-- 	     -- only enable eslint if root has .eslintrc.js
-			-- 	     condition = function(utils)
-			-- 	       -- todo: add support for all eslint config file types
-			-- 	       return utils.root_has_file(".eslintrc.js") -- change file extension if you use something else
-			-- 	     end,
-			-- 	  }),
-			-- },
-		},
-    config = true,
-	},
+          config = function ()
+      local nullls = require "null-ls"
+
+      nullls.setup({
+        debug = false,
+        -- Configure sources which are not installable via `mason-null-ls`
+        sources = {
+          -- nullls.builtins.formatting.prismaFmt,
+          nullls.builtins.formatting.terraform_fmt,
+          nullls.builtins.diagnostics.shellcheck,
+          nullls.builtins.code_actions.shellcheck,
+        }
+      })
+    end,
+    keys = {
+      {
+        "<localleader>fm",
+        -- stylua: ignore
+        function() vim.lsp.buf.format { async = true } end,
+        mode = { "n", "v" },
+        { desc = "Format" }
+      },
+    }
+  },
 
 	{
 		"jay-babu/mason-null-ls.nvim",
+     event = { "BufReadPre", "BufNewFile" },
 		dependencies = {
 			"williamboman/mason.nvim",
 			"jose-elias-alvarez/null-ls.nvim",
@@ -29,11 +38,13 @@ return {
 		opts = {
 			-- list of formatters & linters for mason to install
 			ensure_installed = {
-				"prettier", -- ts/js formatter
+				"prettierd", -- ts/js formatter
         "eslint_d", -- ts/js linter
 				"stylua", -- lua formatter
+        "shfmt",
 			},
 			automatic_installation = true,
+      handlers = {}, -- auto register all `ensure_installed` plugins with null-ls
 		},
 	},
 }
