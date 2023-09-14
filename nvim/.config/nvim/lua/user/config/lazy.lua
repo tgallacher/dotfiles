0590d1a8ad088febe41754b5e4acb596d83332c0
Tom Gallacher <tgallacher@users.noreply.github.com>
2023-09-14 17:09:51 +0100

    style(nvim): formatting

--- nvim/.config/nvim/lua/user/config/lazy.lua
+++ nvim/.config/nvim/lua/user/config/lazy.lua
@@ -1,19 +1,19 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system {
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  }
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- note: all `.lua` files are autoloaded from in a folder which is defined here
 -- note: this includes `<folder>/init.lua` files
- require("lazy").setup{
+require("lazy").setup {
   spec = {
     { import = "user.plugins.editor" },
     { import = "user.plugins.ui" },
