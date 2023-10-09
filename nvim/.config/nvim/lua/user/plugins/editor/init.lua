241023cb472504cde398f69c02454dda41c54eef
Tom Gallacher <tgallacher@users.noreply.github.com>
2023-10-09 09:47:58 +0100

    fix(nvim): breaking changes for indent-line plugin

--- nvim/.config/nvim/lua/user/plugins/editor/init.lua
+++ nvim/.config/nvim/lua/user/plugins/editor/init.lua
@@ -7,12 +7,8 @@ return {
   {
     "lukas-reineke/indent-blankline.nvim",
     event = { "BufReadPost", "BufNewFile" },
-    opts = {
-      char = "│",
-      filetype_exclude = { "help", "alpha", "dashboard", "NvimTree", "Trouble", "lazy", "mason" },
-      show_trailing_blankline_indent = false,
-      show_current_context = false,
-    },
+    main = "ibl",
+    opts = {},
   },
 
   {
