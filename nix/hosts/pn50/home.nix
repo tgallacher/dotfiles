62b5211c747e2c54aa18b4fdc0584760b19db311
Tom Gallacher <tgallacher@users.noreply.github.com>
2024-01-25 13:21:17 +0000

    refactor(nix): move alacritty to home manager

--- nix/hosts/pn50/home.nix
+++ nix/hosts/pn50/home.nix
@@ -7,6 +7,73 @@
     pkgs.whatsapp-for-linux
   ];
 
+  programs = {
+    alacritty = {
+      enable = true;
+      package = pkgs-unstable.alacritty;
+      # see: https://alacritty.org/config-alacritty.html
+      settings = {
+        import = [ "~/.config/alacritty/themes/themes/github_dark_default.yaml" ];
+        cursor = {
+          style = {
+            shape = "Block";
+            blinking = "on";
+            vi_mode_style = "on";
+            unfocussed_hollow = true;
+          };
+          vi_mode_style = { shape = "Underline"; };
+        };
+        draw_bold_text_with_bright_colors = true;
+        fullscreen = false;
+        font = {
+          size = 10;
+          normal = {
+            family = "JetBrainsMono Nerd Font";
+            style = "Regular";
+          };
+          bold = {
+            family = "JetBrainsMono Nerd Font";
+            style = "Bold";
+          };
+          italic = {
+            family = "JetBrainsMono Nerd Font";
+            style = "Bold";
+          };
+          # Offset is the extra space around each character. offset.y can be thought of
+          # as modifying the linespacing, and offset.x as modifying the letter spacing.
+          offset = {
+            x = 0;
+            y = 0;
+          };
+          # Glyph offset determines the locations of the glyphs within their cells with
+          # the default being at the bottom. Increase the x offset to move the glyph to
+          # the right, increase the y offset to move the glyph upward.
+          glyph_offset = {
+            x = 0;
+            y = 0;
+          };
+        };
+        key_bindings = [
+          { key = "Key3"; mods = "Alt"; chars = "#"; } # Alt + #
+        ];
+        mouse = {
+          hide_when_typing = true;
+        };
+        save_to_clipboard = true;
+        window = {
+          dimensions = { lines = 3; columns = 3; };
+          padding = { x = 4; y = 4; };
+          # Window decorations
+          # Setting this to false will result in window without borders and title bar.
+          decorations = "full";
+          startup_mode = "Windowed";
+          dynamic_title = true;
+          opacity = 1.0;
+        };
+      };
+    };
+  };
+
   editorconfig = {
     enable = true;
     settings = {
@@ -23,8 +90,8 @@
 
   # Manage Dotfiles
   home.file = {
-    ".config/alacritty" = {
-      source = ../../../alacritty/.config/alacritty;
+    ".alacritty/themes" = {
+      source = ../../../alacritty/.config/alacritty/themes;
       recursive = true;
     };
     ".config/git" = {
