3cc710735e942283e6ab0d2d0e36a3e18092a0b4
Tom Gallacher <tgallacher@users.noreply.github.com>
2024-02-03 09:03:59 +0000

    feat(nix): initial wofi set up

--- nix/hosts/pn50/home.nix
+++ nix/hosts/pn50/home.nix
@@ -548,105 +548,179 @@ in {
     XDG_SESSION_DESKTOP = "Hyprland";
   };
 
+  programs.wofi = {
+    enable = true;
+    package = inputs.nixpkgs-wayland.packages.${system}.wofi;
+    settings = {
+      allow_images = true;
+      allow_markup = true;
+      content_halign = "fill";
+      dynamic_lines = true;
+      filter_rate = 100;
+      gtk_dark = true;
+      halign = "fill";
+      height = "30%";
+      image_size = 32;
+      insensitive = true;
+      location = "center";
+      matching = "fuzzy"; # fuzzy | contains | multi-contains
+      no_actions = true;
+      orientation = "vertical";
+      prompt = "Search...";
+      show = "drun";
+      terminal = "alacritty";
+      width = "50%";
+    };
+    style = ''
+      *{
+      	font-family: "JetBrainsMono Nerd Font";
+      	min-height: 0;
+      	/* set font-size to 100% if font scaling is set to 1.00 using nwg-look */
+      	font-size: 98%;
+      	font-feature-settings: '"zero", "ss01", "ss02", "ss03", "ss04", "ss05", "cv31"';
+      	padding: 0px;
+      	margin-top: 1px;
+      	margin-bottom: 1px;
+      }
+
+      #window {
+        background-color: #323232;
+        border-radius: 8px;
+      }
+
+      #outer-box {
+        padding: 10px;
+      }
+
+      #input {
+        background-color: #323232;
+        /*border: 1px solid #1e1e2e;*/
+        padding: 4px 6px;
+        color: #585b70;
+      }
+
+      #scroll {
+        margin-top: 10px;
+        margin-bottom: 10px;
+      }
+
+      #inner-box {
+        border-radius: 8px;
+      }
+
+      #img {
+        padding-right: 5px;
+      }
+
+      #text {
+        color: white;
+      }
+
+      #text:selected {
+        color: black;
+      }
+
+      #entry {
+        padding: 3px;
+      }
+
+      #entry:selected {
+        background: linear-gradient(90deg, #89b4fa, #b4befe, #89b4fa);
+        border-radius: 8px;
+      }
+
+      #unselected { }
+
+      #selected { }
+
+      #input, #entry:selected {
+        border-radius: 10px;
+        border: 1px solid #b4befe;
+      }
+    '';
+    # style = ''
+    #   window {
+    #       margin: 0px;
+    #       border: 5px solid #1e1e2e;
+    #       background-color: #cdd6f4;
+    #       border-radius: 0px;
+    #   }
+
+    #   #input {
+    #       padding: 4px;
+    #       margin: 4px;
+    #       padding-left: 20px;
+    #       border: none;
+    #       color: #cdd6f4;
+    #       font-weight: bold;
+    #       background-color: #1e1e2e;
+    #      	outline: none;
+    #       border-radius: 15px;
+    #       margin: 10px;
+    #       margin-bottom: 2px;
+    #   }
+    #   #input:focus {
+    #       border: 0px solid #1e1e2e;
+    #       margin-bottom: 0px;
+    #   }
+
+    #   #inner-box {
+    #       margin: 4px;
+    #       border: 10px solid #1e1e2e;
+    #       color: #cdd6f4;
+    #       font-weight: bold;
+    #       background-color: #1e1e2e;
+    #       border-radius: 15px;
+    #   }
+
+    #   #outer-box {
+    #       margin: 0px;
+    #       border: none;
+    #       border-radius: 15px;
+    #       background-color: #1e1e2e;
+    #   }
+
+    #   #scroll {
+    #       margin-top: 5px;
+    #       border: none;
+    #       border-radius: 15px;
+    #       margin-bottom: 5px;
+    #       /* background: rgb(255,255,255); */
+    #   }
+
+    #   #img:selected {
+    #       background-color: #89b4fa;
+    #       border-radius: 15px;
+    #   }
+
+    #   #text:selected {
+    #       color: #cdd6f4;
+    #       margin: 0px 0px;
+    #       border: none;
+    #       border-radius: 15px;
+    #       background-color: #89b4fa;
+    #   }
+
+    #   #entry {
+    #       margin: 0px 0px;
+    #       border: none;
+    #       border-radius: 15px;
+    #       background-color: transparent;
+    #   }
+
+    #   #entry:selected {
+    #       margin: 0px 0px;
+    #       border: none;
+    #       border-radius: 15px;
+    #       background-color: #89b4fa;
+    #   }
+    # '';
+  };
+
   # Manage Dotfiles
   home.file = {
     # TODO: Better way to get flake location?
     ".config/nvim/".source = config.lib.file.mkOutOfStoreSymlink "${vars.homedir}/Code/${vars.username}/dotfiles/nvim";
-    ".config/wofi/config".text = ''
-      width=1024
-      height=400
-      location=center
-      show=drun
-      prompt=Search...
-      filter_rate=100
-      allow_markup=true
-      no_actions=true
-      halign=fill
-      orientation=vertical
-      content_halign=fill
-      insensitive=true
-      allow_images=true
-      image_size=32
-      gtk_dark=true
-      dynamic_lines=true
-    '';
-    ".config/wofi/style.css".text = ''
-      window {
-          margin: 0px;
-          border: 5px solid #1e1e2e;
-          background-color: #cdd6f4;
-          border-radius: 0px;
-      }
-
-      #input {
-          padding: 4px;
-          margin: 4px;
-          padding-left: 20px;
-          border: none;
-          color: #cdd6f4;
-          font-weight: bold;
-          background-color: #1e1e2e;
-         	outline: none;
-          border-radius: 15px;
-          margin: 10px;
-          margin-bottom: 2px;
-      }
-      #input:focus {
-          border: 0px solid #1e1e2e;
-          margin-bottom: 0px;
-      }
-
-      #inner-box {
-          margin: 4px;
-          border: 10px solid #1e1e2e;
-          color: #cdd6f4;
-          font-weight: bold;
-          background-color: #1e1e2e;
-          border-radius: 15px;
-      }
-
-      #outer-box {
-          margin: 0px;
-          border: none;
-          border-radius: 15px;
-          background-color: #1e1e2e;
-      }
-
-      #scroll {
-          margin-top: 5px;
-          border: none;
-          border-radius: 15px;
-          margin-bottom: 5px;
-          /* background: rgb(255,255,255); */
-      }
-
-      #img:selected {
-          background-color: #89b4fa;
-          border-radius: 15px;
-      }
-
-      #text:selected {
-          color: #cdd6f4;
-          margin: 0px 0px;
-          border: none;
-          border-radius: 15px;
-          background-color: #89b4fa;
-      }
-
-      #entry {
-          margin: 0px 0px;
-          border: none;
-          border-radius: 15px;
-          background-color: transparent;
-      }
-
-      #entry:selected {
-          margin: 0px 0px;
-          border: none;
-          border-radius: 15px;
-          background-color: #89b4fa;
-      }
-    '';
   };
 
   # Let Home Manager manage itself
