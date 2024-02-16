49b9c8bba28b04ce7eaa1b3e778d7b24795f2141
Tom Gallacher <tgallacher@users.noreply.github.com>
2024-02-16 10:13:03 +0000

    chore: reset swaylock theme'

--- nix/libs/hyprland/home/swaylock.nix
+++ nix/libs/hyprland/home/swaylock.nix
@@ -21,9 +21,10 @@
       # Fade in time;
       fade-in = 1;
       # Effect for background;
-      effect-blur = "5x2";
+      effect-blur = "25x20";
       # effect-greyscale;
       # effect-pixelate=5;
+
       # Show/Hide indicator circle;
       indicator = true;
       # smaller indicator;
@@ -32,36 +33,34 @@
       # indicator-radius=300;
       indicator-thickness = 20;
       indicator-caps-lock = true;
+
       # Define all colors;
-      key-hl-color = 00000066;
-      separator-color = 00000000;
-      inside-color = 00000033;
-      inside-clear-color = "ffffff00";
-      inside-caps-lock-color = "ffffff00";
-      inside-ver-color = "ffffff00";
-      inside-wrong-color = "ffffff00";
-      ring-color = "ffffff";
-      ring-clear-color = "ffffff";
-      ring-caps-lock-color = "ffffff";
-      ring-ver-color = "ffffff";
-      ring-wrong-color = "ffffff";
-
-      line-color = 00000000;
-      line-clear-color = "ffffffFF";
-      line-caps-lock-color = "ffffffFF";
-      line-ver-color = "ffffffFF";
-      line-wrong-color = "ffffffFF";
-
-      text-color = "ffffff";
-      text-clear-color = "ffffff";
-      text-ver-color = "ffffff";
-      text-wrong-color = "ffffff";
-
-      bs-hl-color = "ffffff";
-      caps-lock-key-hl-color = "ffffffFF";
-      caps-lock-bs-hl-color = "ffffffFF";
-      disable-caps-lock-text = true;
-      text-caps-lock-color = "ffffff";
+      # key-hl-color = 00000066;
+      # separator-color = 00000000;
+      # inside-color = 00000033;
+      # inside-clear-color = "ffffff00";
+      # inside-caps-lock-color = "ffffff00";
+      # inside-ver-color = "ffffff00";
+      # inside-wrong-color = "ffffff00";
+      # ring-color = "ffffff";
+      # ring-clear-color = "ffffff";
+      # ring-caps-lock-color = "ffffff";
+      # ring-ver-color = "ffffff";
+      # ring-wrong-color = "ffffff";
+      # line-color = 00000000;
+      # line-clear-color = "ffffffFF";
+      # line-caps-lock-color = "ffffffFF";
+      # line-ver-color = "ffffffFF";
+      # line-wrong-color = "ffffffFF";
+      # text-color = "000000";
+      # text-clear-color = "00ff00";
+      # text-ver-color = "0000ff";
+      # text-wrong-color = "ff0000";
+      # bs-hl-color = "fff0f0";
+      # caps-lock-key-hl-color = "000000FF";
+      # caps-lock-bs-hl-color = "000000FF";
+      # disable-caps-lock-text = true;
+      # text-caps-lock-color = "ffff00";
     };
   };
 }
