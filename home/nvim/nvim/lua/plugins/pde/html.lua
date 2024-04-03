43fe8393eedc6b4f6e82cb6b4f81db1b031ac3a3
Tom Gallacher <tgallacher@users.noreply.github.com>
2024-04-03 10:05:22 +0100

    feat(nvim): add html pde config

---
+++ home/nvim/nvim/lua/plugins/pde/html.lua
@@ -0,0 +1,50 @@
+local servers = {
+  cssls = {},
+  html = {
+    filetypes = { "html", "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+  },
+  -- https://github.com/emmetio/emmet
+  emmet_ls = {
+    filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less" },
+    init_options = {
+      html = {
+        options = { ["bem.enabled"] = true },
+      },
+    },
+  },
+}
+
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = vim.list_extend(opts.ensure_installed, { "html", "css" })
+      return opts
+    end,
+  },
+
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = vim.list_extend(opts.ensure_installed, vim.list_extend(vim.tbl_keys(servers), { "prettierd", "html-lsp", "emmet-ls" }))
+      return opts
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      return vim.tbl_deep_extend("force", opts, { servers = servers })
+    end,
+  },
+
+  { -- Autoformat
+    "stevearc/conform.nvim",
+    opts = function(_, opts)
+      return vim.tbl_deep_extend("force", opts, { formatters_by_ft = {
+        html = { "prettierd" },
+        css = { "prettierd" },
+      } })
+    end,
+  },
+}
