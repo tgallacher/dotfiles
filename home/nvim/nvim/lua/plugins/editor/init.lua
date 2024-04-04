8eb0d76a5820f08b8d4b40cdcabd270e67b7a465
Tom Gallacher <tgallacher@users.noreply.github.com>
2024-04-04 21:30:11 +0100

    feat(nvim): better commenting on html-like files

--- home/nvim/nvim/lua/plugins/editor/init.lua
+++ home/nvim/nvim/lua/plugins/editor/init.lua
@@ -38,8 +38,23 @@ return {
     dependencies = { "nvim-treesitter" },
   },
 
-  -- "gc" to comment visual regions/lines
-  { "numToStr/Comment.nvim", opts = {} },
+  -- "gc<motion>" to comment visual regions/lines
+  {
+    "numToStr/Comment.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "JoosepAlviste/nvim-ts-context-commentstring",
+    },
+    config = function()
+      local comment = require("Comment")
+      local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+      --- @diagnostic disable-next-line: missing-fields
+      comment.setup({
+        -- commenting tsx, jsx, html, etc
+        pre_hook = ts_context_commentstring.create_pre_hook(),
+      })
+    end,
+  },
 
   { -- Autoformat
     "stevearc/conform.nvim",
