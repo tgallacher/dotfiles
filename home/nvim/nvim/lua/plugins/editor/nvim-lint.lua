d3d101c8f67cedf0b02aaaa6821b4f4a37fe692e
Tom Gallacher <tgallacher@users.noreply.github.com>
2024-04-04 21:43:15 +0100

    feat(nvim): add nvim-lint bones

---
+++ home/nvim/nvim/lua/plugins/editor/nvim-lint.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      linters_by_ft = {}, -- added in individual PDE files
+    },
+    config = function(_, opts)
+      local lint = require("lint")
+
+      lint.linters_by_ft = opts.linters_by_ft
+
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+        group = vim.api.nvim_create_augroup("lint", { clear = true }),
+        callback = function()
+          lint.try_lint()
+        end,
+      })
+
+      -- stylua: ignore
+      vim.keymap.set("n", "<localleader>ll", function() lint.try_lint() end, { desc = "Trigger [l]inting for current file" })
+    end,
+  },
+}
