05aef34e80cc770754c7b830a38934faa209ef7d
Tom Gallacher <tgallacher@users.noreply.github.com>
2024-04-04 21:31:27 +0100

    fix(nvim): cmp integration with autopairs

--- home/nvim/nvim/lua/plugins/editor/init.lua
+++ home/nvim/nvim/lua/plugins/editor/init.lua
@@ -112,17 +112,18 @@ return {
     end,
   },
 
-  -- autoclose parens, brackets, quotes, etc
-  -- TODO: copy config to CMP:
-  -- https://github.com/windwp/nvim-autopairs#you-need-to-add-mapping-cr-on-nvim-cmp-setupcheck-readmemd-on-nvim-cmp-repo
-  {
+  { -- autoclose parens, brackets, quotes, etc
     "windwp/nvim-autopairs",
     event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/nvim-cmp",
+    },
     opts = {
-      check_ts = true,
-      ts_config = {
+      check_ts = true, -- treesitter not typescript
+      ts_config = { -- ignore these treesitter nodes, per filetype
         lua = { "string", "source" },
         javascript = { "string", "template_string" },
+        java = false,
       },
       disable_filetype = { "TelescopePrompt", "spectre_panel" },
       fast_wrap = {
@@ -138,6 +139,15 @@ return {
         highlight_grey = "LineNr",
       },
     },
+    config = function(_, opts)
+      local autopairs = require("nvim-autopairs")
+      autopairs.setup(opts)
+
+      -- insert `(` after select function or method item
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      local cmp = require("cmp")
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    end,
   },
 
   {
