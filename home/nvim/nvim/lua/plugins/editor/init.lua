8a635096a4aff8588b3904af79dbefb2ebb14706
Tom Gallacher <tgallacher@users.noreply.github.com>
2024-06-13 19:20:58 +0100

    feat(nvim): add nvim dap

--- home/nvim/nvim/lua/plugins/editor/init.lua
+++ home/nvim/nvim/lua/plugins/editor/init.lua
@@ -201,6 +201,31 @@ return {
     },
   },
 
+  { "mfussenegger/nvim-dap" },
+
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio",
+    },
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end,
+  },
+
   -- {
   --   "wolffiex/shellbot",
   --   -- name = "chatgpt",
@@ -241,3 +266,4 @@ return {
   --   "dhruvasagar/vim-table-mode",
   -- },
 }
+
