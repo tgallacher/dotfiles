ece4eaa38945d912099650069286270d58540df5
Tom Gallacher <tgallacher@users.noreply.github.com>
2024-06-14 12:27:20 +0100

    chore(nvim): clean comments on lualine

--- home/nvim/nvim/lua/plugins/ui/lualine.lua
+++ home/nvim/nvim/lua/plugins/ui/lualine.lua
@@ -1,6 +1,3 @@
--- local colors = require("catppuccin.palettes.mocha")
--- local color_utils = require("catppuccin.utils.colors")
-
 -- Display if buffer has been Harpoon'd
 --
 -- source: https://github.com/dmmulroy/kickstart.nix
@@ -25,16 +22,9 @@ return {
   { -- statusline
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    -- FIXME: Overwrites default startup dashboard and injects `[no name]` buffer.
-    -- Can use Alpha plugin as a workaround to control custom dashboard.
-    --
-    -- enabled = false,
-    -- event = "StdinReadPre",
-    -- lazy = true,
     opts = function()
       return {
         options = {
-          theme = "rose-pine",
           icons_enabled = true,
           component_separators = { left = "", right = "" }, -- remove default separators
           section_separators = { left = "", right = "" }, -- remove default separators
@@ -45,25 +35,13 @@ return {
         },
         sections = {
           lualine_a = {
-            {
-              "mode",
-              -- separator = { left = "", right = "" },
-              -- color = {
-              --   fg = colors.base,
-              --   bg = colors.mauve,
-              -- },
-            },
+            { "mode" },
           },
           lualine_b = {
             harpoon,
             {
               "buffers",
               use_mode_colors = false,
-              -- separator = { left = "", right = "" },
-              -- buffers_color = {
-              --   active = { fg = colors.mauve, bg = colors.mantle },
-              --   inactive = { fg = colors.overlay0, bg = colors.mantle },
-              -- },
             },
           },
           lualine_c = {},
@@ -71,8 +49,6 @@ return {
             {
               "diff",
               draw_empty = true,
-              -- separator = { left = "", right = "" },
-              -- color = { bg = colors.surface0 },
               -- diff_colors = {
               --   added = colors.green,
               --   modified = colors.yellow,
@@ -83,29 +59,14 @@ return {
             {
               "branch",
               icon = "󰘬",
-              -- separator = { left = "", right = "" },
-              -- color = {
-              --   -- fg = colors.text,
-              --   -- -- bg = color_utils.darken(colors.mauve, 0.55),
-              --   -- fg = colors.overlay2,
-              --   -- bg = colors.base,
-              -- },
             },
           },
           lualine_y = {
-            -- {
-            --   "filesize",
-            --   separator = { left = "", right = "" },
-            --   -- color = {
-            --   --   fg = colors.overlay2,
-            --   --   bg = colors.crust,
-            --   -- },
-            -- },
+            -- { "filesize", },
             {
               "diagnostics",
               sources = { "nvim_lsp", "nvim_diagnostic" },
               sections = { "error", "warn" },
-              -- separator = { left = "", right = "" },
               symbols = { error = " ", warn = " " },
               -- diagnostics_colors = {
               --   error = colors.red,
@@ -114,36 +75,15 @@ return {
               colored = true,
               update_in_insert = false,
               always_visible = true,
-              -- color = {
-              --   bg = colors.mantle,
-              -- },
             },
           },
           lualine_z = {
             {
               "filetype",
-              -- separator = { left = "", right = "" },
               colored = false, -- icon only
-              -- color = {
-              --   fg = colors.overlay1,
-              --   bg = colors.base,
-              -- },
             },
-            {
-              "progress",
-              -- separator = { left = "", right = "" },
-              -- color = {
-              --   fg = colors.subtext1,
-              --   bg = colors.surface0,
-              -- },
-            },
-            {
-              "location",
-              -- color = {
-              --   fg = colors.text,
-              --   bg = colors.surface1,
-              -- },
-            },
+            { "progress" },
+            { "location" },
           },
         },
         extensions = {
