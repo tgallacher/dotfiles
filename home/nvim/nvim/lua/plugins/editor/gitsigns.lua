ce747ac7e594133ceb7de4812c36e935aa754b3e
Tom Gallacher <173261044+tomg-flwls@users.noreply.github.com>
2024-08-13 16:45:41 +0100

    chore(nvim): git blame on by default

--- home/nvim/nvim/lua/plugins/editor/gitsigns.lua
+++ home/nvim/nvim/lua/plugins/editor/gitsigns.lua
@@ -3,12 +3,12 @@ return {
     "lewis6991/gitsigns.nvim",
     event = "VeryLazy",
     opts = {
-      current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
       current_line_blame_opts = {
         virt_text = true,
         virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
         delay = 1000,
-        ignore_whitespace = false,
+        ignore_whitespace = true,
       },
       on_attach = function(bufnr)
         local gs = package.loaded.gitsigns
