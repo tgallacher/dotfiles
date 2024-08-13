a135d63a138bf7755654e1cbde308cedbfc4ed76
Tom Gallacher <173261044+tomg-flwls@users.noreply.github.com>
2024-08-13 16:54:24 +0100

    chore(nvim): clean up colorscheme comments

--- home/nvim/nvim/lua/plugins/ui/colorscheme.lua
+++ home/nvim/nvim/lua/plugins/ui/colorscheme.lua
@@ -7,39 +7,31 @@ return {
     priority = 1000,
     opts = {
       variant = "main",
-      -- dim_inactive_windows = true,
-      styles = {
-        transparency = true,
-      },
+      styles = { transparency = true },
     },
     config = function(_, opts)
       require("rose-pine").setup(opts)
 
       vim.cmd.colorscheme("rose-pine")
-
-      -- -- vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#ea9a97", bg = "#6e6a86" })
-      -- vim.api.nvim_set_hl(0, "Cursor", { fg = "#ea9a97", bg = "#6e6a86" })
-      -- vim.api.nvim_set_hl(0, "CursorLine", { bg = "#26233a" })
-      -- vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#ea9a97", bg = "#26233a" })
-      -- -- vim.api.nvim_set_hl(0, "CursorColumn", { fg = "#ea9a97", bg = "#6e6a86" })
     end,
   },
 
   {
     "EdenEast/nightfox.nvim",
+    enabled = true,
+    lazy = false,
+    priority = 1000,
     opts = {
       options = {
         -- transparent = true,
-        styles = {
-          comments = "italic",
-        },
+        styles = { comments = "italic" },
       },
     },
     config = function(_, opts)
       require("nightfox").setup(opts)
 
+      -- `terafox` also looks really good
       vim.cmd.colorscheme("carbonfox")
-      -- vim.cmd.colorscheme("terafox")
     end,
   },
 }
