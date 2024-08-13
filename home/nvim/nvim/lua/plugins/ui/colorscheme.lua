d39af0dfb1c8768845073a720741d62fb911fa81
Tom Gallacher <173261044+tomg-flwls@users.noreply.github.com>
2024-08-13 16:51:38 +0100

    chore(nvim): remove unused colorscheme fn

--- home/nvim/nvim/lua/plugins/ui/colorscheme.lua
+++ home/nvim/nvim/lua/plugins/ui/colorscheme.lua
@@ -1,21 +1,3 @@
-local base = {
-  red = "#ff657a",
-  maroon = "#F29BA7",
-  peach = "#ff9b5e",
-  yellow = "#eccc81",
-  green = "#a8be81",
-  teal = "#9cd1bb",
-  sky = "#A6C9E5",
-  sapphire = "#86AACC",
-  blue = "#5d81ab",
-  lavender = "#66729C",
-  mauve = "#b18eab",
-}
-
-local extend_base = function(value)
-  return vim.tbl_extend("force", base, value)
-end
-
 return {
   {
     "rose-pine/neovim",
