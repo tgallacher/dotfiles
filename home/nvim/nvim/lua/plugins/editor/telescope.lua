84dab5fca7f7a70e1f2fe298d2f7c1f0b56eb13c
Tom Gallacher <173261044+tomg-flwls@users.noreply.github.com>
2024-08-14 11:50:41 +0100

    feat(nvim): setup fzf sorter in telescope

--- home/nvim/nvim/lua/plugins/editor/telescope.lua
+++ home/nvim/nvim/lua/plugins/editor/telescope.lua
@@ -34,6 +34,12 @@ return {
           },
         },
         extensions = {
+          fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = "smart_case",
+          },
           ["ui-select"] = {
             require("telescope.themes").get_dropdown(),
           },
