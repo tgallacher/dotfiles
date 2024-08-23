fe3d896286d27b33d167aed8a6927242e3d2861e
Tom Gallacher <173261044+tomg-flwls@users.noreply.github.com>
2024-08-23 07:48:04 +0100

    feat(term): configure kitty

--- home/terminal/kitty.nix
+++ home/terminal/kitty.nix
@@ -7,11 +7,17 @@
   programs.kitty = {
     enable = true;
     package = upkgs.kitty;
-    # font = {
-    #   normal = {
-    #     family = "JetBrainsMono Nerd Font";
-    #     style = "Regular";
-    #   };
-    # };
+    theme = "Nightfox";
+    shellIntegration.enableZshIntegration = true;
+    font = {
+      name = "JetBrainsMono Nerd Font";
+      size = 12;
+    };
+    settings = {
+      macos_option_as_alt = "both";
+      macos_quit_when_last_window_closed = "yes";
+      hide_window_decorations = "titlebar-only";
+      window_margin_width = 5;
+    };
   };
 }
