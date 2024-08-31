ad88a10a8ce6b0ffde5ca124f2ffdd2d02923e0b
Tom Gallacher <173261044+tomg-flwls@users.noreply.github.com>
2024-08-31 06:48:48 +0100

    chore(nvim): switch to terrafox colorshceme

--- home/nvim/nvim/lua/plugins/ui/colorscheme.lua
+++ home/nvim/nvim/lua/plugins/ui/colorscheme.lua
@@ -1,22 +1,5 @@
 return {
   {
-    "rose-pine/neovim",
-    enabled = false,
-    name = "rose-pine",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      variant = "main",
-      styles = { transparency = true },
-    },
-    config = function(_, opts)
-      require("rose-pine").setup(opts)
-
-      vim.cmd.colorscheme("rose-pine")
-    end,
-  },
-
-  {
     "EdenEast/nightfox.nvim",
     enabled = true,
     lazy = false,
@@ -30,10 +13,9 @@ return {
     config = function(_, opts)
       require("nightfox").setup(opts)
 
-      -- `terafox` also looks really good
       -- vim.cmd.colorscheme("carbonfox")
-      -- vim.cmd.colorscheme("terafox")
-      vim.cmd.colorscheme("nightfox")
+      vim.cmd.colorscheme("terafox")
+      -- vim.cmd.colorscheme("nightfox")
     end,
   },
 }
