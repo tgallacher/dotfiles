1fd1b16d0346baa032b7c84552fbc358510142da
Tom Gallacher <173261044+tomg-flwls@users.noreply.github.com>
2024-08-31 06:57:17 +0100

    chore(term): tweak kitty config; fix osx pound key

--- home/terminal/kitty.nix
+++ home/terminal/kitty.nix
@@ -11,10 +11,14 @@
     shellIntegration.enableZshIntegration = true;
     font = {
       name = "JetBrainsMono Nerd Font";
-      size = 12;
+      size = 13;
     };
+    extraConfig = ''
+      map ctrl+shift+6
+      map alt+3           send_text application #
+    '';
     settings = {
-      macos_option_as_alt = "both";
+      macos_option_as_alt = "both"; # must restart kitty for this to take effect
       macos_quit_when_last_window_closed = "yes";
       hide_window_decorations = "titlebar-only";
       window_margin_width = 5;
