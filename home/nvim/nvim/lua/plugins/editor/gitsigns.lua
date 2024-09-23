ebfff1a27b321c7ee3443ae862087dbae4063119
Tom Gallacher <tgallacher@users.noreply.github.com>
2024-09-23 14:18:04 +0100

    feat(nvim): add vim fugitive

--- home/nvim/nvim/lua/plugins/editor/gitsigns.lua
+++ home/nvim/nvim/lua/plugins/editor/gitsigns.lua
@@ -1,4 +1,7 @@
 return {
+  -- Embedded Git commands inside neovim
+  { "tpope/vim-fugitive" },
+
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     "lewis6991/gitsigns.nvim",
     event = "VeryLazy",
