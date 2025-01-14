0a7b626e2b72508dea4b333ca00d5f36c4a547db
Tom Gallacher <tgallacher@users.noreply.github.com>
2025-01-14 21:07:55 +0000

    fix(prompt): split starship on 2 lines to avoid wrapping on tmux panes

--- home/terminal/prompt.nix
+++ home/terminal/prompt.nix
@@ -8,34 +8,35 @@
       git_commit.only_detached = false;
       git_metrics.disabled = false;
       # palette = "catppuccin_${flavour}";
-      format = ''$username$hostname$directory$git_branch$git_state$git_status $cmd_duration$line_break$character'';
-      right_format = ''$localip$kubernetes$docker_context$package$golang$lua$nodejs$pulumi$rust$terraform$python$nix_shell$aws$gcloud$direnv$sudo$container'';
-      container.format = "[$symbol \[$name\]]($style) ";
+      # format = ''$username$hostname$directory$git_branch$git_state$git_status $cmd_duration$line_break$character'';
+      # right_format = ''$localip$kubernetes$docker_context$package$golang$lua$nodejs$pulumi$rust$terraform$python$nix_shell$aws$gcloud$direnv$sudo$container'';
+      format = "$all$hostname$directory$git_branch$git_commit$git_state$git_metrics$git_status";
+      # container.format = "[$symbol \[$name\]]($style) ";
       # directory.style = "blue";
-      character = {
-        success_symbol = "[❯](purple)";
-        error_symbol = "[❯](red)";
-        vimcmd_symbol = "[❮](green)";
-      };
-      git_branch = {
-        format = "[$branch]($style)";
-        # style = "bright-black";
-      };
-      git_status = {
-        format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style)";
-        # style = "cyan";
-        conflicted = "";
-        untracked = "";
-        modified = "";
-        staged = "";
-        renamed = "";
-        deleted = "";
-        stashed = "≡";
-      };
-      git_state = {
-        format = "\([$state( $progress_current/$progress_total)]($style)\) ";
-        # style = "bright-black";
-      };
+      # character = {
+      #   success_symbol = "[❯](purple)";
+      #   error_symbol = "[❯](red)";
+      #   vimcmd_symbol = "[❮](green)";
+      # };
+      # git_branch = {
+      #   format = "[$branch]($style)";
+      #   # style = "bright-black";
+      # };
+      # git_status = {
+      #   format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style)";
+      #   # style = "cyan";
+      #   conflicted = "";
+      #   untracked = "";
+      #   modified = "";
+      #   staged = "";
+      #   renamed = "";
+      #   deleted = "";
+      #   stashed = "≡";
+      # };
+      # git_state = {
+      #   format = "\([$state( $progress_current/$progress_total)]($style)\) ";
+      #   # style = "bright-black";
+      # };
       cmd_duration = {
         format = "[$duration]($style) ";
         # style = "yellow";
