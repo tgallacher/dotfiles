d50af58c21d82e901e826b194ec3c267d2f55735
Tom Gallacher <tgallacher@users.noreply.github.com>
2025-01-15 10:29:07 +0000

    feat(nvim): add basic go pde

---
+++ home/nvim/nvim/lua/plugins/pde/go.lua
@@ -0,0 +1,35 @@
+local servers = {
+  gopls = {},
+}
+
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = vim.list_extend(opts.ensure_installed, { "go" })
+      return opts
+    end,
+  },
+
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = vim.list_extend(opts.ensure_installed, vim.list_extend(vim.tbl_keys(servers), { "gofumpt" }))
+      return opts
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      return vim.tbl_deep_extend("force", opts, { servers = servers })
+    end,
+  },
+
+  { -- Autoformat
+    "stevearc/conform.nvim",
+    opts = function(_, opts)
+      return vim.tbl_deep_extend("force", opts, { formatters_by_ft = { lua = { "gofumpt" } } })
+    end,
+  },
+}
