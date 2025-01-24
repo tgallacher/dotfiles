16a3ae7009cc5422f2d6b74363a9433ae25fffc0
Tom Gallacher <tgallacher@users.noreply.github.com>
2025-01-24 13:57:55 +0000

    chore(nvim): tmp disable deno pde due to conflict with typescript pde

--- home/nvim/nvim/lua/plugins/pde/deno.lua
+++ home/nvim/nvim/lua/plugins/pde/deno.lua
@@ -4,51 +4,54 @@ local servers = {
   denols = {},
 }
 
+-- TODO: Add note to call out what this does and why we need to set it
 vim.g.markdown_fenced_languages = {
   "ts=typescript",
 }
 
-return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "javascript",
-        "typescript",
-        "tsx",
-      })
-    end,
-  },
+return {}
 
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    opts = function(_, opts)
-      opts.ensure_installed = vim.list_extend(opts.ensure_installed, vim.list_extend(vim.tbl_keys(servers), {}))
-      return opts
-    end,
-  },
-
-  -- { -- Lint
-  --   "mfussenegger/nvim-lint",
-  --   opts = function(_, opts)
-  --     return vim.tbl_deep_extend("force", opts, {
-  --       linters_by_ft = {
-  --         typescript = { "biomejs" },
-  --         -- javascript = { "biomejs" },
-  --       },
-  --     })
-  --   end,
-  -- },
-
-  -- { -- Autoformat
-  --   "stevearc/conform.nvim",
-  --   opts = function(_, opts)
-  --     return vim.tbl_deep_extend("force", {
-  --       formatters_by_ft = {
-  --         javascript = { "biome" },
-  --         typescript = { "biome" },
-  --       },
-  --     }, opts)
-  --   end,
-  -- },
-}
+-- return {
+--   {
+--     "nvim-treesitter/nvim-treesitter",
+--     opts = function(_, opts)
+--       vim.list_extend(opts.ensure_installed, {
+--         "javascript",
+--         "typescript",
+--         "tsx",
+--       })
+--     end,
+--   },
+--
+--   {
+--     "WhoIsSethDaniel/mason-tool-installer.nvim",
+--     opts = function(_, opts)
+--       opts.ensure_installed = vim.list_extend(opts.ensure_installed, vim.list_extend(vim.tbl_keys(servers), {}))
+--       return opts
+--     end,
+--   },
+--
+--   -- { -- Lint
+--   --   "mfussenegger/nvim-lint",
+--   --   opts = function(_, opts)
+--   --     return vim.tbl_deep_extend("force", opts, {
+--   --       linters_by_ft = {
+--   --         typescript = { "biomejs" },
+--   --         -- javascript = { "biomejs" },
+--   --       },
+--   --     })
+--   --   end,
+--   -- },
+--
+--   -- { -- Autoformat
+--   --   "stevearc/conform.nvim",
+--   --   opts = function(_, opts)
+--   --     return vim.tbl_deep_extend("force", {
+--   --       formatters_by_ft = {
+--   --         javascript = { "biome" },
+--   --         typescript = { "biome" },
+--   --       },
+--   --     }, opts)
+--   --   end,
+--   -- },
+-- }
