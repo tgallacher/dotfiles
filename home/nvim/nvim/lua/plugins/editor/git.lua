8d79361469e9c67397753d3484b04ca89c0a0b23
Tom Gallacher <tgallacher@users.noreply.github.com>
2025-01-24 13:56:20 +0000

    feat(nvim): add git conflict plugin

--- home/nvim/nvim/lua/plugins/editor/git.lua
+++ home/nvim/nvim/lua/plugins/editor/git.lua
@@ -1,5 +1,27 @@
 return {
-  { -- Embedded Git commands inside neovim
+  {
+    -- highlight git merge conflict marker sections
+    {
+      "rhysd/conflict-marker.vim",
+      init = function()
+        -- Set the conflict marker highlight group to an empty string
+        vim.g.conflict_marker_highlight_group = ""
+        -- Include text after begin and end markers
+        vim.g.conflict_marker_begin = "^<<<<<<< .*$"
+        vim.g.conflict_marker_end = "^>>>>>>> .*$"
+        -- FIXME: adjust the colours so they match the current colorscheme
+        -- Highlight settings
+        vim.cmd([[
+        highlight ConflictMarkerBegin guifg=#e06c75
+        highlight ConflictMarkerOurs guibg=#2e5049
+        highlight ConflictMarkerSeparator guifg=#e06c75
+        highlight ConflictMarkerTheirs guibg=#344f69
+        highlight ConflictMarkerEnd guifg=#e06c75
+        ]])
+      end,
+    },
+
+    -- Embedded Git commands inside neovim
     "tpope/vim-fugitive",
     init = function()
       vim.keymap.set("n", "<leader>gs", ":Git<cr>", { desc = "[g]it [s]tatus" })
