06573ca08ed322b84741736d5cb29d386fc92d5a
Tom Gallacher <tgallacher@users.noreply.github.com>
2025-02-10 15:01:52 +0000

    chore(term): adjust font wezterm font

--- home/terminal/wezterm/config.lua
+++ home/terminal/wezterm/config.lua
@@ -19,8 +19,8 @@ return {
   },
   -- Fonts
   font = wezterm.font("JetBrains Mono", { weight = "Regular" }),
-  font_size = 13,
-  line_height = 1.10,
+  font_size = 14,
+  line_height = 1.00,
   use_dead_keys = false,
   -- Appearance
   hide_tab_bar_if_only_one_tab = true,
