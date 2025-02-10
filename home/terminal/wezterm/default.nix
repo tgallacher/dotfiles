b59e8d8a63e491d0e3b024471a8425cf82ca6079
Tom Gallacher <tgallacher@users.noreply.github.com>
2025-02-10 15:01:28 +0000

    fix(term): tmp mitigate wezterm issue with neovim terminal

--- home/terminal/wezterm/default.nix
+++ home/terminal/wezterm/default.nix
@@ -6,6 +6,7 @@
   programs.wezterm = {
     enable = true;
     package = upkgs.wezterm;
+    enableZshIntegration = false; # FIXME: https://github.com/wezterm/wezterm/issues/5007
     extraConfig = ''
       -- load external config file so we can get syntax highlighting
       local config = require("config")
