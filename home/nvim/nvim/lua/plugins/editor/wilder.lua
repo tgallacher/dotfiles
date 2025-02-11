6c01e4135efd4428779b2e63a584e08786858083
Tom Gallacher <tgallacher@users.noreply.github.com>
2025-02-11 15:03:13 +0000

    chore(nvim): wilder menu fuzzy matching and highlighting
    Also switched to python over vimgrep for better perf

--- home/nvim/nvim/lua/plugins/editor/wilder.lua
+++ home/nvim/nvim/lua/plugins/editor/wilder.lua
@@ -20,16 +20,31 @@ return {
 
       -- Enable fuzzy matching for commands and buffers
       wilder.set_option("pipeline", {
-        wilder.branch(wilder.cmdline_pipeline({ fuzzy = 1 }), wilder.vim_search_pipeline({ fuzzy = 1 })),
+        wilder.branch(
+          wilder.cmdline_pipeline({ language = "python", fuzzy = 1, set_pcre2_pattern = 1 }),
+          wilder.python_search_pipeline({ pattern = "fuzzy" })
+          -- wilder.vim_search_pipeline({ fuzzy = 1 })
+        ),
       })
+
+      -- local highlighters = {
+      --   wilder.pcre2_highlighter(),
+      --   wilder.basic_highlighter(),
+      -- }
 
       wilder.set_option(
         "renderer",
         wilder.wildmenu_renderer({
+          -- [":"] = wilder.popupmenu_renderer({
+          --   highlighter = highlighters,
+          -- }),
+          -- ["/"] = wilder.wildmenu_renderer({
+          --   highlighter = highlighters,
+          -- }),
           highlighter = wilder.basic_highlighter(),
-          -- highlights = {
-          --   accent = wilder.make_hl("WilderAccent", "Pmenu", { { a = 1 }, { a = 1 }, { foreground = palette.red } }),
-          -- },
+          highlights = {
+            accent = wilder.make_hl("WilderAccent", "Pmenu", { { a = 1 }, { a = 1 }, { foreground = "#C34043" } }),
+          },
         })
       )
 
