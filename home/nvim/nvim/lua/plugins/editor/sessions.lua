19790a578e533f5e802df247f492556b37b6bac2
Tom Gallacher <tgallacher@users.noreply.github.com>
2025-02-11 15:03:43 +0000

    fix(nvim): ensure ft and hightlight plugins work on session restore

--- home/nvim/nvim/lua/plugins/editor/sessions.lua
+++ home/nvim/nvim/lua/plugins/editor/sessions.lua
@@ -6,12 +6,16 @@ return {
       local auto_session = require("auto-session")
 
       auto_session.setup({
-        auto_restore_enabled = false, -- let us manually restore this
-        auto_session_suppress_dirs = { "~/", "~/Code/", "~/Downloads", "~/Documents", "~/Desktop/" },
+        auto_restore = false, -- let us manually restore this
+        suppressed_dirs = { "~/", "~/Code/", "~/Downloads", "~/Documents", "~/Desktop/" },
       })
 
       vim.keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "[W]orkspace session [R]estore for cwd" }) -- restore last workspace session for current directory
       vim.keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "[W]orkspace [S]ave session for cwd" }) -- save workspace session for current working directory
+    end,
+    init = function()
+      -- ensure's filetype, highlighting plugins work on restore
+      vim.opt.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
     end,
   },
 }
