7d75b62d471b5c076bba6fca4c513131e59174a2
Tom Gallacher <tgallacher@users.noreply.github.com>
2025-02-21 08:37:16 +0000

    chore(nvim): refactor session plugin

--- home/nvim/nvim/lua/plugins/editor/sessions.lua
+++ home/nvim/nvim/lua/plugins/editor/sessions.lua
@@ -2,20 +2,17 @@ return {
   { -- save nvim session and restore
     -- NOTE: saving is automatic, but reloading is manual
     "rmagatti/auto-session",
-    config = function()
-      local auto_session = require("auto-session")
-
-      auto_session.setup({
-        auto_restore = false, -- let us manually restore this
-        suppressed_dirs = { "~/", "~/Code/", "~/Downloads", "~/Documents", "~/Desktop/" },
-      })
-
-      vim.keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "[W]orkspace session [R]estore for cwd" }) -- restore last workspace session for current directory
-      vim.keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "[W]orkspace [S]ave session for cwd" }) -- save workspace session for current working directory
-    end,
+    lazy = false,
+    opts = {
+      auto_restore = false, -- let us manually restore this
+      suppressed_dirs = { "~/", "~/Code/", "~/Downloads", "~/Documents", "~/Desktop/" },
+    },
     init = function()
       -- ensure's filetype, highlighting plugins work on restore
       vim.opt.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+
+      vim.keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "[W]orkspace session [R]estore for cwd" }) -- restore last workspace session for current directory
+      vim.keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "[W]orkspace [S]ave session for cwd" }) -- save workspace session for current working directory
     end,
   },
 }
