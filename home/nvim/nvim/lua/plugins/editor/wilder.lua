202dd60bcc3a446ba0023d0abe63294892023187
Tom Gallacher <tgallacher@users.noreply.github.com>
2025-02-24 14:50:11 +0000

    chore(nvim): tweak wilder search
    Still need to figure out how to get fuzzy matching on git branches from
    within vim-fugitive

--- home/nvim/nvim/lua/plugins/editor/wilder.lua
+++ home/nvim/nvim/lua/plugins/editor/wilder.lua
@@ -21,9 +21,27 @@ return {
       -- Enable fuzzy matching for commands and buffers
       wilder.set_option("pipeline", {
         wilder.branch(
+          wilder.python_file_finder_pipeline({
+            -- to use ripgrep : {'rg', '--files'}
+            -- to use fd      : {'fd', '-tf'}
+            file_command = { "fd", "-tf" },
+            dir_command = { "fd", "-td" },
+            -- use {'cpsm_filter'} for performance, requires cpsm vim plugin
+            -- found at https://github.com/nixprime/cpsm
+            filters = { "fuzzy_filter", "difflib_sorter" },
+          }),
           wilder.cmdline_pipeline({ language = "python", fuzzy = 1, set_pcre2_pattern = 1 }),
-          -- wilder.python_search_pipeline({ pattern = "fuzzy" })
-          wilder.vim_search_pipeline({ fuzzy = 1 })
+          wilder.python_search_pipeline({
+            -- pattern = "fuzzy"
+            -- can be set to wilder#python_fuzzy_delimiter_pattern() for stricter fuzzy matching
+            pattern = wilder.python_fuzzy_pattern(),
+            -- omit to get results in the order they appear in the buffer
+            sorter = wilder.python_difflib_sorter(),
+            -- can be set to 're2' for performance, requires pyre2 to be installed
+            -- see :h wilder#python_search() for more details
+            engine = "re",
+          })
+          -- wilder.vim_search_pipeline({ fuzzy = 1 })
         ),
       })
 
