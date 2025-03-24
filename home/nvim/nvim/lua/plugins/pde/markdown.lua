96c6213438ed2ee7fa72757288d99171e3fff2e6
Tom Gallacher <tgallacher@users.noreply.github.com>
2025-03-24 07:07:15 +0000

    chore(nvim): bring back markdown preview

--- home/nvim/nvim/lua/plugins/pde/markdown.lua
+++ home/nvim/nvim/lua/plugins/pde/markdown.lua
@@ -117,4 +117,13 @@ return {
       })
     end,
   },
+
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
 }
