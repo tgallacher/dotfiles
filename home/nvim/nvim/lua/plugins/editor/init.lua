4187021c52985fdc95e9d0c113f615e1db193096
Tom Gallacher <tgallacher@users.noreply.github.com>
2025-05-15 15:22:16 +0100

    feat(nvim): bind harpoon view w/preview to <C-e>

--- home/nvim/nvim/lua/plugins/editor/init.lua
+++ home/nvim/nvim/lua/plugins/editor/init.lua
@@ -157,8 +157,32 @@ return {
       local harpoon = require("harpoon")
 
       harpoon:setup(opts)
+    end,
+    init = function()
+      local harpoon = require("harpoon")
+
+      -- basic telescope configuration
+      local conf = require("telescope.config").values
+      local function toggle_telescope(harpoon_files)
+        local file_paths = {}
+        for _, item in ipairs(harpoon_files.items) do
+          table.insert(file_paths, item.value)
+        end
+
+        require("telescope.pickers")
+          .new({}, {
+            prompt_title = "Harpoon",
+            finder = require("telescope.finders").new_table({
+              results = file_paths,
+            }),
+            previewer = conf.file_previewer({}),
+            sorter = conf.generic_sorter({}),
+          })
+          :find()
+      end
 
       --stylua: ignore start
+      vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end, { desc = "Open Harpoon window" })
       vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end, {desc = "[H]arpoon [A]dd File" })
       vim.keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, {desc = "[H]arpoon [T]oggle File Menu" })
 
