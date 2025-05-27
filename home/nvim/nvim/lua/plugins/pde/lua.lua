6e4a48acdfcf40b22c1b3cc2dd5d16184347c89b
Tom Gallacher <tgallacher@users.noreply.github.com>
2025-05-27 21:59:35 +0100

    chore(nvim): remove duep keymaps

--- home/nvim/nvim/lua/plugins/pde/lua.lua
+++ home/nvim/nvim/lua/plugins/pde/lua.lua
@@ -102,22 +102,13 @@ return {
       }
 
       dap.adapters.nlua = function(callback, config)
-        callback({ type = "server", host = config.host or "127.0.0.1", port = config.port or 8086 })
+        callback({
+          type = "server",
+          host = config.host or "127.0.0.1",
+          port = config.port or 8086,
+        })
       end
 
-      vim.keymap.set("n", "<leader>dl", function()
-        require("osv").launch({ port = 8086 })
-      end, { noremap = true })
-
-      vim.keymap.set("n", "<leader>dw", function()
-        local widgets = require("dap.ui.widgets")
-        widgets.hover()
-      end)
-
-      vim.keymap.set("n", "<leader>df", function()
-        local widgets = require("dap.ui.widgets")
-        widgets.centered_float(widgets.frames)
-      end)
       vim.keymap.set("n", "<leader>dl", function()
         require("osv").launch({ port = 8086 })
       end, { noremap = true })
