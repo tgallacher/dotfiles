9df8d923078bf846c6bc3e68e3af039cce059065
Tom Gallacher <tgallacher@users.noreply.github.com>
2025-07-19 00:35:34 +0100

    wip: add yaml and json lsp configs

--- home/nvim/nvim/lua/plugins/pde/json.lua
+++ home/nvim/nvim/lua/plugins/pde/json.lua
@@ -1,54 +1,38 @@
--- FIXME: Update to nvim 0.11 setup when this LSP is required
-local servers = {
-  jsonls = {
-    settings = {
-      json = {
-        -- FIXME: causing errors on every edit to the file
-        -- lazy-load schemastore when needed
-        -- on_new_config = function(new_config)
-        --   new_config.settings.json.schemas = new_config.settings.json.schemas or {}
-        --   vim.list_extend(new_config.settings.json.schemas, require("schemastore").json.schemas())
-        -- end,
-        format = { enable = true },
-        validate = { enable = true },
-      },
-    },
-  },
-}
+vim.lsp.enable({ "jsonls" })
+
 -- account for first time bootup, and the schemastore plugin hasn't been installed yet
 local isOk, schemastore = pcall(require, "schemastore")
 if isOk then
-  servers.jsonls.settings.json.schemas = schemastore.json.schemas()
+  vim.lsp.jsonls.settings.json.schemas = schemastore.json.schemas()
 end
 
-return {} -- tmp disable
--- return {
---   {
---     "b0o/SchemaStore.nvim",
---     lazy = true,
---     version = false, -- last release too old
---   },
---
---   {
---     "nvim-treesitter/nvim-treesitter",
---     opts = function(_, opts)
---       opts.ensure_installed = vim.list_extend(opts.ensure_installed, { "json", "json5", "jsonc" })
---       return opts
---     end,
---   },
---
---   {
---     "WhoIsSethDaniel/mason-tool-installer.nvim",
---     opts = function(_, opts)
---       opts.ensure_installed = vim.list_extend(opts.ensure_installed, vim.list_extend(vim.tbl_keys(servers), { "prettierd" }))
---       return opts
---     end,
---   },
---
---   { -- Autoformat
---     "stevearc/conform.nvim",
---     opts = function(_, opts)
---       return vim.tbl_deep_extend("force", opts, { formatters_by_ft = { json = { "prettierd" } } })
---     end,
---   },
--- }
+return {
+  {
+    "b0o/SchemaStore.nvim",
+    lazy = true,
+    version = false, -- last release too old
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = vim.list_extend(opts.ensure_installed, { "json", "json5", "jsonc" })
+      return opts
+    end,
+  },
+
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = vim.list_extend(opts.ensure_installed, { "jsonls", "prettierd" })
+      return opts
+    end,
+  },
+
+  { -- Autoformat
+    "stevearc/conform.nvim",
+    opts = function(_, opts)
+      return vim.tbl_deep_extend("force", opts, { formatters_by_ft = { json = { "prettierd" } } })
+    end,
+  },
+}
--- home/nvim/nvim/lua/plugins/pde/yaml.lua
+++ home/nvim/nvim/lua/plugins/pde/yaml.lua
@@ -1,71 +1,38 @@
--- FIXME: Update to nvim 0.11 setup when this LSP is required
-local servers = {
-  yamlls = {
-    -- Have to add this for yamlls to understand that we support line folding
-    capabilities = {
-      textDocument = {
-        foldingRange = {
-          dynamicRegistration = false,
-          lineFoldingOnly = true,
-        },
-      },
-    },
-    -- lazy-load schemastore when needed
-    on_new_config = function(new_config)
-      new_config.settings.yaml.schemas = vim.tbl_deep_extend("force", new_config.settings.yaml.schemas or {}, require("schemastore").yaml.schemas())
-    end,
-    settings = {
-      redhat = { telemetry = { enabled = false } },
-      yaml = {
-        keyOrdering = false,
-        format = { enable = true },
-        validate = true,
-        schemaStore = {
-          -- Must disable built-in schemaStore support to use
-          -- schemas from SchemaStore.nvim plugin
-          enable = false,
-          -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
-          url = "",
-        },
-      },
-    },
-  },
-}
+vim.lsp.enable({ "yamlls" })
 
 -- account for first time bootup, and the schemastore plugin hasn't been installed yet
 local isOk, schemastore = pcall(require, "schemastore")
 if isOk then
-  servers.yamlls.settings.yaml.schemas = schemastore.yaml.schemas()
+  vim.lsp.yamlls.settings.yaml.schemas = schemastore.yaml.schemas()
 end
 
-return {} -- tmp disable
--- return {
---   {
---     "b0o/SchemaStore.nvim",
---     lazy = true,
---     version = false, -- last release is way too old
---   },
---
---   {
---     "nvim-treesitter/nvim-treesitter",
---     opts = function(_, opts)
---       opts.ensure_installed = vim.list_extend(opts.ensure_installed, { "yaml" })
---       return opts
---     end,
---   },
---
---   {
---     "WhoIsSethDaniel/mason-tool-installer.nvim",
---     opts = function(_, opts)
---       opts.ensure_installed = vim.list_extend(opts.ensure_installed, vim.list_extend(vim.tbl_keys(servers), { "prettierd" }))
---       return opts
---     end,
---   },
---
---   { -- Autoformat
---     "stevearc/conform.nvim",
---     opts = function(_, opts)
---       return vim.tbl_deep_extend("force", opts, { formatters_by_ft = { yaml = { "prettierd" } } })
---     end,
---   },
--- }
+return {
+  {
+    "b0o/SchemaStore.nvim",
+    lazy = true,
+    version = false, -- last release is way too old
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = vim.list_extend(opts.ensure_installed, { "yaml" })
+      return opts
+    end,
+  },
+
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = vim.list_extend(opts.ensure_installed, { "yamlls", "prettierd" })
+      return opts
+    end,
+  },
+
+  { -- Autoformat
+    "stevearc/conform.nvim",
+    opts = function(_, opts)
+      return vim.tbl_deep_extend("force", opts, { formatters_by_ft = { yaml = { "prettierd" } } })
+    end,
+  },
+}
