4a0e7b52ad7629c9ace56918502ec4b82f62ff12
Tom Gallacher <tgallacher@users.noreply.github.com>
2025-08-18 20:42:18 +0100

    chore(nvim): enable bashls with nvim 0.11

--- home/nvim/nvim/lua/plugins/pde/sh.lua
+++ home/nvim/nvim/lua/plugins/pde/sh.lua
@@ -1,34 +1,33 @@
--- FIXME: Update to nvim 0.11 setup when this LSP is required
-local servers = {
-  bashls = {},
-}
+vim.lsp.enable({ "bashls" })
 
-return {} -- tmp disable
--- return {
---   {
---     "nvim-treesitter/nvim-treesitter",
---     opts = function(_, opts)
---       opts.ensure_installed = vim.list_extend(opts.ensure_installed, { "bash" })
---       return opts
---     end,
---   },
---
---   {
---     "WhoIsSethDaniel/mason-tool-installer.nvim",
---     opts = function(_, opts)
---       opts.ensure_installed = vim.list_extend(opts.ensure_installed, vim.list_extend(vim.tbl_keys(servers), { "shfmt", "shellcheck" }))
---       return opts
---     end,
---   },
---
---   { -- Autoformat
---     "stevearc/conform.nvim",
---     opts = function(_, opts)
---       return vim.tbl_deep_extend("force", opts, {
---         formatters_by_ft = {
---           sh = { "shfmt" },
---         },
---       })
---     end,
---   },
--- }
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = vim.list_extend(opts.ensure_installed, { "bash" })
+      return opts
+    end,
+  },
+
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = vim.list_extend(opts.ensure_installed, {
+        "shfmt",
+        "shellcheck",
+      })
+      return opts
+    end,
+  },
+
+  { -- Autoformat
+    "stevearc/conform.nvim",
+    opts = function(_, opts)
+      return vim.tbl_deep_extend("force", opts, {
+        formatters_by_ft = {
+          sh = { "shfmt" },
+        },
+      })
+    end,
+  },
+}
